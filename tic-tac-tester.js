2091e668ac6127586e4bcf773d628617dcc4e1f5
yisheng90 <yisheng@yishenglee.com>
2016-12-12 13:58:17 +0800

    first try

--- tic-tac-tester.js
+++ tic-tac-tester.js
@@ -1,24 +1,3 @@
-/*global isGameOver, whoWon, playTurn, restart */
-/* Tic-Tac-Tester.js
-
-This script will test the game logic of your tic tac toe game. To use it you will need to include it in your html file after you main tic-tac-toe script. You will need to declare the following functions in the global scope:
-
-# playTurn(index)
-It should take one parameter which is a zero-based index to your grid, indicating where the current player's token should be played.
-It should return a boolean value to indicate whether the move was allowed or not - true if it was successful, false otherwise e.g. if the square is already taken or the game is over.
-
-# isGameOver()
-It should return a true or false if the game is over.
-
-# whoWon()
-It should return 0 if the game is not yet finished. Else it should return either 1 or 2 depending on which player one. It should return 3 if the game is a draw.
-
-# restart()
-It should restart the game so it can be played again.
-
-It is assumed that the turns of the player will be automatically changed after an allowed move.
-
-The application will console log all the passed or failed test */
 
 // CONFIG
 var showPasses = true
@@ -50,6 +29,7 @@ simulateGame('P1 TL to BR Victory', [0, 2, 4, 5, 8], playerOne)
 simulateGame('P1 TL to BR Victory', [1, 0, 2, 4, 5, 8], playerTwo)
 simulateGame('P2 TR to BL Victory', [2, 1, 4, 3, 6], playerOne)
 simulateGame('P2 TR to BL Victory', [0, 2, 1, 4, 3, 6], playerTwo)
+simulateGame('Draw Game', [0, 1, 2, 3, 5, 4, 6, 8, 7], playerDraw)
 simulateGame('Draw Game', [0, 1, 2, 3, 5, 4, 6, 8, 7], playerDraw)
 
 console.log('==================================')
